f574b49d1e9073ff5f74234b8056a63d1b0c2d11
君陌离 <289097246@qq.com>
2025-03-19 10:31:29 +0800

    发布于：2025-03-19 10:31:02

---
+++ assets/stateDiagram-v2-OC7PCC74-BnamMXSV.js
@@ -0,0 +1,1 @@
+import{a as s,B as t,r as a,I as e}from"./chunk-HOLNC4VV-D8fQozpZ.js";import"./chunk-BWM7Q77P-CID8jKue.js";import{m as o}from"./mermaid.esm.min-qx24aiau.js";import"./app-Ciqs_Xc9.js";var l={parser:e,db:a,renderer:t,styles:s,init:o(r=>{r.state||(r.state={}),r.state.arrowMarkerAbsolute=r.arrowMarkerAbsolute,a.clear()},"init")};export{l as diagram};
---
+++ js/vue2mp3.js
@@ -0,0 +1,132 @@
+setTimeout(() => {
+  let ele = document.createElement("div");
+  ele.id = "myapp";
+  document.body.appendChild(ele);
+  new Vue({
+    el: "#myapp",
+    hide: true,
+    props: {
+      list: {
+        type: Array,
+        default: () => {
+          return [
+            { id: 1, text: 11 },
+            { id: 2, text: 22 },
+          ];
+        },
+      },
+    },
+    data() {
+      return {
+        params: {
+          text: 1,
+          record: {
+            id: 2,
+          },
+          row: {
+            index: 0,
+          },
+        },
+      };
+    },
+
+    render(createElement) {
+      let that = this;
+      if (this.qq) {
+        return createElement(
+          "ul",
+          this.list.map((item, index) => {
+            return createElement("li", [
+              createElement(
+                "h2",
+                {
+                  on: {
+                    click: () => {
+                      this.clickHandler(4444, item);
+                    },
+                  },
+                },
+                "slot传参方式"
+              ),
+              "slot传参",
+              createElement(
+                "h3",
+                this.$scopedSlots.test(item.text, item, index)
+              ),
+              createElement(
+                "h4",
+                {
+                  class: "h4-container",
+                  on: {
+                    click: function () {
+                      that.clickHandler(index, item);
+                    },
+                  },
+                },
+                "测试h4"
+              ),
+              createElement(
+                "a-divider",
+                {
+                  props: {
+                    type: "horizontal",
+                  },
+                  class: "qq-container",
+                  style: { background: "#ccc" },
+                  on: {
+                    click: () => {
+                      console.log("🚀", 12321);
+                    },
+                  },
+                },
+                "测试"
+              ),
+              createElement(
+                "a-button",
+                {
+                  props: {
+                    type: "primary",
+                  },
+                  style: { margin: "10px 0" },
+                  class: "qq-container",
+                  on: {
+                    click: () => {
+                      console.log("🚀测试button🚀", index, item);
+                    },
+                  },
+                },
+                "测试button"
+              ),
+            ]);
+          })
+        );
+      } else {
+        return createElement(
+          "p",
+          {
+            props: {
+              type: "primary",
+            },
+            style: { display:'none' },
+            class: "p-container",
+            on: {
+              click: () => {
+                console.log("🚀测试🚀");
+              },
+            },
+          },
+          "No Lists Found."
+        );
+      }
+    },
+    methods: {
+      clickHandler(index, item) {
+        console.log("🚀 ~ clickHandler ~ index,item:", index, item);
+        this.$message.info(`Click ${index}`);
+      },
+    },
+    mounted() {
+      // console.log("🚀", this);
+    },
+  });
+}, 1000);
